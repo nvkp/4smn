0db3e0653041a50cb4dcefc5d904ffeabd914d98
Petr Novák <novp19@vse.cz>
2021-01-31 16:02:20 +0100

    pentest a siem

--- en.search.min.5a86144598ea252bbe41f4ca45f68421eb1be001db7f4c1d8d2dab547231390b.js
+++ en.search.min.5a86144598ea252bbe41f4ca45f68421eb1be001db7f4c1d8d2dab547231390b.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/4smn/flexsearch.min.js');loadScript('/4smn/en.search-data.min.78591f25b3ebb81e066f2cc4bc9d852bf64b8415aceb4d385b90925a734f99ff.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/4smn/flexsearch.min.js');loadScript('/4smn/en.search-data.min.e22628ead7fc225808c67e8595b458469ebdb74795c6c7d4cf7411912ddb61fd.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
