739b04098e09dfebafa099934551502df224c969
Petr Novák <novp19@vse.cz>
2021-02-01 21:31:05 +0100

    bezpecnostni tymy

--- en.search.min.26d323b4564b6302f5e1bc9af3d2b70c876c5fa1158d6275d46204e3f1d8df02.js
+++ en.search.min.26d323b4564b6302f5e1bc9af3d2b70c876c5fa1158d6275d46204e3f1d8df02.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/4smn/flexsearch.min.js');loadScript('/4smn/en.search-data.min.c78ed1e47dce5f315d8d70362b5bc54a2a597f62964adf2d84bb079738d921a2.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/4smn/flexsearch.min.js');loadScript('/4smn/en.search-data.min.5bc6eb6b68634e1d836d7cd78e1b7943d0751192769ecc43ae410b8e0ff58954.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
